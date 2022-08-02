67504a4caa8ef66603bc323d43d3083f4907bb7b
Ericky121 <ericky@naver.com>
2022-08-02 17:25:24 +0900

    2022.08.02 First Changed

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -5,6 +5,7 @@ import 'package:gap/gap.dart';
 
 import '../utils/app_layout.dart';
 import '../widgets/icon_text_widget.dart';
+import '../widgets/ticket_tabs.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -26,51 +27,7 @@ class SearchScreen extends StatelessWidget {
             style: Styles.headLineStyle1.copyWith(fontSize: 35),
           ),
           Gap(AppLayout.getHeight(20)),
-          FittedBox(
-            child: Container(
-              padding: EdgeInsets.all(3.5),
-              child: Row(
-                children: [
-                  /*
-                  airline ticket
-                   */
-                  Container(
-                    width: size.width * .44,
-                    padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.horizontal(left: Radius.circular(AppLayout.getHeight(7))),
-                      color: Colors.white,
-                    ),
-                    child: Center(
-                      child: Text(
-                        "Airline tickets"
-                      ),
-                    ),
-                  ),
-                  /*
-                  hotels
-                   */
-                  Container(
-                    width: size.width * .44,
-                    padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(7)),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.horizontal(right: Radius.circular(AppLayout.getHeight(7))),
-                      color: Colors.transparent,
-                    ),
-                    child: Center(
-                      child: Text(
-                          "Hotels"
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: Color(0xFFF4F6FD),
-              ),
-            ),
-          ),
+          AppTicketTabs(firstTab: "Airline tickets", secondTab: "Hotels",),
           Gap(AppLayout.getHeight(25)),
           AppIconText(
             icon: Icons.flight_takeoff_rounded,
@@ -96,6 +53,130 @@ class SearchScreen extends StatelessWidget {
           ),
           Gap(AppLayout.getHeight(25)),
           AppDoubleTextWidget(bigText: "Upcoming Flights", smallText: "View all"),
+          Gap(AppLayout.getHeight(15)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              /* First container */
+              Container(
+                height: AppLayout.getHeight(400),
+                width: size.width * 0.42,
+                padding: EdgeInsets.symmetric(horizontal: AppLayout.getHeight(15), vertical: AppLayout.getWidth(15)),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.shade200,
+                        blurRadius: 1,
+                        spreadRadius: 1
+                    )
+                  ]
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      height: AppLayout.getHeight(190),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            "assets/images/sit.jpg"
+                          )
+                        )
+                      ),
+                    ),
+                    Gap(AppLayout.getHeight(12)),
+                    Text(
+                      "20% discount on the early booking of this flight. Don't miss out the chance.",
+                      style: Styles.headLineStyle2,
+                    ),
+                  ],
+                ),
+              ),
+
+              /* Second container */
+              Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: size.width * 0.44,
+                        height: AppLayout.getHeight(200),
+                        decoration: BoxDecoration(
+                          color: Color(0xFF3A8888),
+                          borderRadius: BorderRadius.circular(18),
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(15), horizontal: AppLayout.getHeight(15)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Discount\nfor survey",
+                              style: Styles.headLineStyle2.copyWith(fontWeight: FontWeight.bold, color: Colors.white),
+                            ),
+                            Gap(AppLayout.getHeight(10)),
+                            Text(
+                              "Take the survey about our services and get discount",
+                              style: Styles.headLineStyle2.copyWith(fontWeight: FontWeight.w500, color: Colors.white, fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        right: -45,
+                        top: -40,
+                        child: Container(
+                          padding: EdgeInsets.all(AppLayout.getHeight(30)),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(width: 18, color: Color(0xFF189999)),
+                            color: Colors.transparent,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Gap(AppLayout.getHeight(15)),
+                  Container(
+                    width: size.width * 0.44,
+                    height: AppLayout.getHeight(200),
+                    padding: EdgeInsets.symmetric(vertical: AppLayout.getHeight(15), horizontal: AppLayout.getHeight(15)),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Color(0xFFEC6545),
+                    ),
+                    child: Column(
+                      children: [
+                        Text("Taks love", style: Styles.headLineStyle2.copyWith(color: Colors.white, fontWeight: FontWeight.bold),),
+                        Gap(AppLayout.getHeight(5)),
+                        RichText(
+                          text: TextSpan(
+                            children: [
+                              TextSpan(
+                                text: "A",
+                                style: TextStyle(fontSize: 38),
+                              ),
+                              TextSpan(
+                                text: "B",
+                                style: TextStyle(fontSize: 50),
+                              ),
+                              TextSpan(
+                                text: "C",
+                                style: TextStyle(fontSize: 38),
+                              ),
+                            ]
+                          )
+                        )
+                      ],
+                    ),
+                  )
+
+                ],
+              )
+            ],
+          )
 
         ],
       ),
---
+++ lib/screens/ticket_screen.dart
@@ -0,0 +1,18 @@
+import 'package:booktickets/utils/app_layout.dart';
+import 'package:flutter/material.dart';
+
+class TicketScreen extends StatelessWidget {
+  const TicketScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
+    return Scaffold(
+      body: ListView(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
